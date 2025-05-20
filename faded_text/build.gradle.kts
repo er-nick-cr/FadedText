15a3158874b0efecbe9e6d829ef36e83c735784c
er-nick-cr <nikiteremeev2010@gmail.com>
2025-05-20 20:01:50 +0700

    Initial commit

---
+++ faded_text/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "com.github.ernickcr.faded_text"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 24
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.ui)
+    implementation(libs.ui.graphics)
+    implementation(libs.material3)
+}
